e302d919210d8edaa2475beaa4bfe88c43e26e12
Reductions <slavi.boyanov@gmail.com>
2017-09-06 16:42:41 +0300

    Adding Absinthe deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,10 @@ defmodule Clio.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:absinthe, "~> 1.3"},
+      {:absinthe_plug, "~> 1.3"},
+      {:absinthe_ecto, "~> 0.1.2"}
     ]
   end
 
