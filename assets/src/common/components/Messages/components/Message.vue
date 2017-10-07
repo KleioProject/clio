6dd498cf57ef53909b15b108be8ac0793320643a
Mechoshi Puhanaga <mechokio@gmail.com>
2017-10-07 19:18:27 +0300

    z-index variables; messages styling edited

--- assets/src/common/components/Messages/components/Message.vue
+++ assets/src/common/components/Messages/components/Message.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="message-component" :class="{success: message.isSuccess}">
         <div class="tcenter">
-            <h3 :class="{'icon-ok': message.isSuccess, 'icon-attention': !message.isSuccess}">{{message.headline}}</h3>
-            <p class="tcenter">{{message.body}}</p>
+            <h3 class="message-headline" :class="{'icon-ok': message.isSuccess, 'icon-attention': !message.isSuccess}">{{message.headline}}</h3>
+            <p class="tcenter message-body">{{message.body}}</p>
         </div>
         <div class="date-time" v-html="$options.filters.datetime(message.timestamp,'time')"></div>
         <div class="button-group">
-            <button @click="close">Close</button>
+            <button class="btn-small" @click="close">Close</button>
         </div>
     </div>
 </template>
